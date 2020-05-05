6b0a46195dbe4366003fb9c227ab9644da5ebdc1
Xsanori <elxsanori@gmail.com>
2020-05-05 16:39:11 +0300

    Add repo

--- master.sh
+++ master.sh
@@ -83,6 +83,9 @@ function PrepareSystem {
         echo $LANG_INSTALLING
         echo ""
         sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip git imagemagick openjdk-8-jdk python maven git-lfs meld ghex libssl-dev libncurses5 && sudo apt-get autoremove -y
+        curl https://storage.googleapis.com/git-repo-downloads/repo > repo
+        chmod a+x repo
+        sudo mv repo /bin/repo
     fi
 }
 
